aeae65c8c97fab030de6606dfb10a16a921b36e4
Sylvain Leroux <sylvain@chicoree.fr>
2014-02-23 13:09:05 +0100

    Use year/episode# to infer movie or TV show

--- batch.py
+++ batch.py
@@ -1,22 +1,31 @@
 import os
+import re
 import fileinput
 from shlex import quote as quote
 
 for line in fileinput.input():
-    volume, iso, title, episode, name = (item.strip() 
+    volume, iso, title, episode_or_year, name = (item.strip() 
                                 for item in line.split("|"))
 
-    cmd = "python3 main.py --volume {volume} \
-                           --episode {episode} \
-                           --title {name} \
-                           --dvd-device {iso} \
-                           --target '/usr/local/share/xbmc/TV Shows' \
-                           {title}"
+    if re.match("\d{4}",episode_or_year):
+        cmd = "python3 main.py --volume {volume} \
+                               --year {episode_or_year} \
+                               --dvd-device {iso} \
+                               --target '/usr/local/share/xbmc/Movies' \
+                               {title}"
+        
+    else:
+        cmd = "python3 main.py --volume {volume} \
+                               --episode {episode_or_year} \
+                               --title {name} \
+                               --dvd-device {iso} \
+                               --target '/usr/local/share/xbmc/TV Shows' \
+                               {title}"
+
     cmd = cmd.format(volume=quote(volume),
                      iso=quote(iso),
                      name=quote(name),
                      title=quote(title),
-                     episode=quote(episode))
-
+                     episode_or_year=quote(episode_or_year))
     os.system(cmd)
 
