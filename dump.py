9cac1189419009a29eedd41102752ee8211feff7
Sylvain Leroux <sylvain@chicoree.fr>
2015-05-03 22:47:30 +0200

    Support to tag file names
    Useful for TV series when several DVDs have the same title

--- dump.py
+++ dump.py
@@ -66,13 +66,14 @@ def run_and_display(cmd, **kwargs):
     display(pipe(cmd, **kwargs))
 
 def make_lst_file(lsdvd):
-    fmt = "{name:25s} | {title:>12s}.ISO | dvd://{ix:<3d} | 2000 | # {h:3d}:{m:02d}:{s:04.1f}"
+    fmt = "{name:25s} | {fname:>12s}.ISO | dvd://{ix:<3d} | 2000 | # {h:3d}:{m:02d}:{s:04.1f}"
     threshold = 60*4 # four minutes
 
+    fname = lsdvd['file']
     title = lsdvd['title']
     name = title.replace("_"," ").title()
 
-    fname = "{title}.LST".format(title=title)
+    fname = "{fname}.LST".format(fname=fname)
 
     with open(fname, "at") as f:
         if f.tell() > 0:
@@ -86,11 +87,14 @@ def make_lst_file(lsdvd):
             if length > threshold:
                 m, s = divmod(length, 60)
                 h, m = divmod(m, 60)
-                print(fmt.format(title=title, name=name, ix=ix, h=int(h), m=int(m), s=s),file=f)
+                print(fmt.format(fname=fname, name=name, ix=ix, h=int(h), m=int(m), s=s),file=f)
             
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--tag", 
+                            help="Extra tag used when naming output file",
+                            default="")
     parser.add_argument("device", 
                             help="The block device containing the media",
                             nargs='?',
@@ -108,8 +112,13 @@ def main():
     lsdvd = ast.literal_eval(rawdata)
 
     title = lsdvd['title']
+    if args.tag:
+        lsdvd['file'] = "-".join((title, args.tag))
+    else:
+        lsdvd['file'] = title
+
     make_lst_file(lsdvd)
-    run(DDRESCUE, device=device, title=title)
+    run(DDRESCUE, device=device, title=lsdvd['file'])
     run(EJECT, device=device)
     
 
